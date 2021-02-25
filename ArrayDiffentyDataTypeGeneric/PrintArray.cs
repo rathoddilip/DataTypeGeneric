145c921a3b5eed4ff8c2c7f0c80905e352e2688e
diliprathod32 <diliprathod32@gmail.com>
2021-02-25 22:57:12 +0530

    [Dilip] PrintingSimpleMethodArrayElement

---
+++ ArrayDiffentyDataTypeGeneric/PrintArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayDiffentyDataTypeGeneric
+{
+   public class PrintArray
+    {
+        public void Toprint(int[] inputArray)
+        {
+            Console.WriteLine("Interge Simple Array");
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+        }
+        public void Toprint(char[] inputArray)
+        {
+            Console.WriteLine("char Simple Array");
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine((char)element);
+            }
+        }
+        public void Toprint(double[] inputArray)
+        {
+            Console.WriteLine("double Simple Array");
+            foreach (int element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            
+        }
+    }
+}
---
+++ ArrayDiffentyDataTypeGeneric/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArrayDiffentyDataTypeGeneric
+{
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintArray printArray = new PrintArray();
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = {10.1D, 20.2D, 30.3D, 40.4D, 50.5D };
+            char[] charArray = { 'W', 'E', 'L', 'C', 'O', 'M', 'E' };
+            printArray.Toprint(intArray);
+            printArray.Toprint(doubleArray);
+            printArray.Toprint(charArray);
+            Console.ReadLine();
+        }
+    }
+}
