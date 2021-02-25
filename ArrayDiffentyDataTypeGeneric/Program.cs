4484d75891d7839c4c67beac03dbed7224bf9764
diliprathod32 <diliprathod32@gmail.com>
2021-02-25 23:00:02 +0530

    [Dilip] PrintingSimpleMethodArrayElement

--- ArrayDiffentyDataTypeGeneric/Program.cs
+++ ArrayDiffentyDataTypeGeneric/Program.cs
@@ -10,6 +10,7 @@ namespace ArrayDiffentyDataTypeGeneric
             int[] intArray = { 1, 2, 3, 4, 5 };
             double[] doubleArray = {10.1D, 20.2D, 30.3D, 40.4D, 50.5D };
             char[] charArray = { 'W', 'E', 'L', 'C', 'O', 'M', 'E' };
+            //Simple Method Array 
             printArray.Toprint(intArray);
             printArray.Toprint(doubleArray);
             printArray.Toprint(charArray);
